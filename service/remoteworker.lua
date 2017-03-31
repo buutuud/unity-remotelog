bc0a14019dc59dc6228d4a555aa23fc0383e3c54
happyyi <buutuud@gmail.com>
2017-03-31 21:59:31 +0800

    delete unused file

--- service/remoteworker.lua
+++ service/remoteworker.lua
@@ -1,8 +1,8 @@
 local skynet = require "skynet"
 local socket = require "socket"
 local md5 = require "md5"
-local lfs = require "lfs"
-local cache = require "cachefile"
+--local lfs = require "lfs"
+--local cache = require "cachefile"
 
 local pvr_path = skynet.getenv "pvr"
 
@@ -57,86 +57,86 @@ end
 --      size of bytes
 -- c<-s NOTFOUND
 -- c<-s ERROR string
-function command.GET(fd, hash)
-	local fullpath = cache.download_name(hash)
-	local sz = lfs.attributes(fullpath, "size")
-	if not sz then
-		socket.write(fd, "NOTFOUND\n")
-		return
-	end
-	local f = io.open(fullpath, "rb")
-	if f == nil then
-		socket.write("ERROR Can't open\n")
-		error("Can't open " .. fullpath)
-	end
-	socket.write(fd, "OK " .. sz .. "\n")
-	local data = f:read "a"
-	f:close()
-	assert(#data == sz , "Invalid size")
-	socket.write(fd, data)
-end
+--function command.GET(fd, hash)
+	--local fullpath = cache.download_name(hash)
+	--local sz = lfs.attributes(fullpath, "size")
+	--if not sz then
+		--socket.write(fd, "NOTFOUND\n")
+		--return
+	--end
+	--local f = io.open(fullpath, "rb")
+	--if f == nil then
+		--socket.write("ERROR Can't open\n")
+		--error("Can't open " .. fullpath)
+	--end
+	--socket.write(fd, "OK " .. sz .. "\n")
+	--local data = f:read "a"
+	--f:close()
+	--assert(#data == sz , "Invalid size")
+	--socket.write(fd, data)
+--end
 
-local function exist_file(fullpath, fd, size)
-	local sz = lfs.attributes(fullpath, "size")
-	if sz then
-		if sz ~= size then
-			socket.write(fd, "ERROR Invalid size\n")
-			error (string.format("Invalid size %d | %d", size, sz))
-		else
-			lfs.touch(fullpath)
-			socket.write(fd, "EXIST\n")
-			return true
-		end
-	end
-	return false
-end
+--local function exist_file(fullpath, fd, size)
+	--local sz = lfs.attributes(fullpath, "size")
+	--if sz then
+		--if sz ~= size then
+			--socket.write(fd, "ERROR Invalid size\n")
+			--error (string.format("Invalid size %d | %d", size, sz))
+		--else
+			--lfs.touch(fullpath)
+			--socket.write(fd, "EXIST\n")
+			--return true
+		--end
+	--end
+	--return false
+--end
 
--- c->s size md5.ext
--- c<-s EXIST
---      OK
--- c->s size of bytes
--- c<-s ERROR string
-function command.PUT(fd, args)
-	local size, hash, ext = args:match("(%d+) ([%da-f]+)%.([%w]+)")
-	if size == nil or hash == nil or ext == nil then
-		socket.write(fd, "ERROR Invalid command\n")
-		error ( "Invalid command " .. args )
-	end
-	size = tonumber(size)
-	local fullpath = cache.upload_name(hash, ext)
-	if exist_file(fullpath, fd, size) then
-		return
-	end
-	socket.write(fd, "OK\n")
-	local data = assert(socket.read(fd, size), "size invalid")
-	local h = md5.sumhexa(data)
-	if h~=hash then
-		socket.write(fd, "ERROR Invalid md5\n")
-		error "Invalid md5"
-	end
-	local tn = os.tmpname()
-	local f = io.open(tn,"wb")
-	if f == nil then
-		socket.write(fd, "ERROR create file failed\n")
-		error ("create " .. tn .. " failed")
-	end
-	f:write(data)
-	f:close()
-	local ok, err = lfs.link(tn, fullpath)
-	local remove_ok, err2 = os.remove(tn)
-	if not remove_ok then
-		skynet.error("Remove failed: " .. tn .. " " .. err2)
-	end
-	if not ok then
-		if exist_file(fullpath, fd, size) then
-			return
-		end
-		socket.write(fd, "ERROR write failed\n")
-		error (err)
-	else
-		socket.write(fd, "OK\n")
-	end
-end
+---- c->s size md5.ext
+---- c<-s EXIST
+----      OK
+---- c->s size of bytes
+---- c<-s ERROR string
+--function command.PUT(fd, args)
+	--local size, hash, ext = args:match("(%d+) ([%da-f]+)%.([%w]+)")
+	--if size == nil or hash == nil or ext == nil then
+		--socket.write(fd, "ERROR Invalid command\n")
+		--error ( "Invalid command " .. args )
+	--end
+	--size = tonumber(size)
+	--local fullpath = cache.upload_name(hash, ext)
+	--if exist_file(fullpath, fd, size) then
+		--return
+	--end
+	--socket.write(fd, "OK\n")
+	--local data = assert(socket.read(fd, size), "size invalid")
+	--local h = md5.sumhexa(data)
+	--if h~=hash then
+		--socket.write(fd, "ERROR Invalid md5\n")
+		--error "Invalid md5"
+	--end
+	--local tn = os.tmpname()
+	--local f = io.open(tn,"wb")
+	--if f == nil then
+		--socket.write(fd, "ERROR create file failed\n")
+		--error ("create " .. tn .. " failed")
+	--end
+	--f:write(data)
+	--f:close()
+	--local ok, err = lfs.link(tn, fullpath)
+	--local remove_ok, err2 = os.remove(tn)
+	--if not remove_ok then
+		--skynet.error("Remove failed: " .. tn .. " " .. err2)
+	--end
+	--if not ok then
+		--if exist_file(fullpath, fd, size) then
+			--return
+		--end
+		--socket.write(fd, "ERROR write failed\n")
+		--error (err)
+	--else
+		--socket.write(fd, "OK\n")
+	--end
+--end
 
 local working = {}
 
@@ -167,27 +167,27 @@ end
 -- c<-s MISSING md5 md5 ...
 -- c<-s OK md5 (result file)
 -- c<-s ERROR string
-function command.TEX(fd, args)	-- pvrtextools
-	local download_hash = md5.sumhexa(args)
-	wait(download_hash)
-	local download_name = cache.download_name(download_hash)
-	local download_size =  lfs.attributes(download_name, "size")
-	if download_size then
-		-- already convert
-		lfs.touch(download_name)
-		skynet.error("Cache file : " .. download_hash)
-		socket.write(fd, "OK " .. download_hash .. "\n")
-		return
-	end
-	working[download_hash] = true
-	socket.abandon(fd)
-	local err = convert_call(fd, args, download_hash)
-	socket.start(fd)
-	wakeup(download_hash)
-	if err then
-		error(err)
-	end
-end
+--function command.TEX(fd, args)	-- pvrtextools
+	--local download_hash = md5.sumhexa(args)
+	--wait(download_hash)
+	--local download_name = cache.download_name(download_hash)
+	--local download_size =  lfs.attributes(download_name, "size")
+	--if download_size then
+		---- already convert
+		--lfs.touch(download_name)
+		--skynet.error("Cache file : " .. download_hash)
+		--socket.write(fd, "OK " .. download_hash .. "\n")
+		--return
+	--end
+	--working[download_hash] = true
+	--socket.abandon(fd)
+	--local err = convert_call(fd, args, download_hash)
+	--socket.start(fd)
+	--wakeup(download_hash)
+	--if err then
+		--error(err)
+	--end
+--end
 
 local function dispatch(cfd)
 	socket.start(cfd)
@@ -215,12 +215,12 @@ end
 
 skynet.start(function()
 	skynet.newservice("debug_console",8000)
-	cache.init(config.path)
+	--cache.init(config.path)
 	local port = skynet.getenv("port") or 8964
 	skynet.error("Listen 0.0.0.0:"..port)
 	local fd = assert(socket.listen("0.0.0.0", port))
 	skynet.error("Worker:"..config.slave)
-	init_convert()
+	--init_convert()
 	socket.start(fd, function(cfd, addr)
 		skynet.error(addr .. " connected")
 		dispatch(cfd)
